42d48176d9ec3333b0d78952e070471ba6d25882
James Pearson Hughes <james@winnie.com>
2022-10-28 15:35:54 -0700

    Maybe fix support for some mypy errors
    There are errors that don't include line number or column number, so
    previously they would get silently ignored.  I _think_ this might fix
    that.

--- script.sh
+++ script.sh
@@ -39,7 +39,7 @@ mypy_check_output="$(mypy --show-column-numbers     \
 
 # shellcheck disable=SC2086
 echo "${mypy_check_output}" | reviewdog              \
-      -efm="%f:%l:%c: %t%*[^:]: %m"                  \
+      -efm="%f:%l?:?%c?:? %t%*[^:]: %m"              \
       -name="${INPUT_TOOL_NAME:-mypy}"               \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}"            \
